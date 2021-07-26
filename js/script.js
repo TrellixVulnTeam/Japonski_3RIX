69e706ad4e4f53a8100134dc6b95302c1d51d0f6
Asya404 <unkiplum@gmail.com>
2021-07-26 20:08:05 +0200

    section-opinie (slider), fixed bg

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+   $(".owl-carousel").owlCarousel({
+      items: 1,
+      loop: true,
+      nav: true,
+      dots: true,
+      navText: ["<i class='opinia__arrow opinia__arrow--l icon-arrows-left'></i>", "<i class='opinia__arrow opinia__arrow--r icon-arrows-right'></i>"]
+   });
+
+});
